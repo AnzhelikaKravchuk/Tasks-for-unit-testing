febe1f34a9a3125b7c1f81e9e4cadd294a333b31
Zhenya Salash <ev.salash@gmail.com>
2020-01-15 16:09:57 +0300

    I have finished task 1, but I am going to add commit a little bit later.

--- Task1/StringHelper.cs
+++ Task1/StringHelper.cs
@@ -6,8 +6,24 @@ namespace Task1
     {
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (str == null)
+            {
+                throw new ArgumentNullException("String cannot be null.");
+            }
+            else if(str=="")
+            {
+                throw new ArgumentException("String cannot be empty.");
+            }
+            int Sum = 0;            
+            char[] letters = str.ToCharArray();
+            foreach (char letter in letters)
+            {
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                {
+                    Sum++;
+                }                
+            }            
+            return Sum;           
         }
     }
 }
