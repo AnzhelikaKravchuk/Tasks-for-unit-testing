f0e774d50cabcad4fc0fb18cbb8b8e7df7d838e4
Zhenya Salash <ev.salash@gmail.com>
2020-01-16 19:59:41 +0300

    I aded FxCopAnalyzers and StyleCope.

--- Task1/StringHelper.cs
+++ Task1/StringHelper.cs
@@ -2,28 +2,48 @@
 
 namespace Task1
 {
+    /// <summary>
+    /// This class is for finding letters.
+    /// </summary>
     public static class StringHelper
     {
+        /// <summary>
+        /// This method is for finding correct letters.
+        /// </summary>
+        /// <param name="str">
+        /// It is a letter.
+        /// </param>
+        /// <returns>
+        /// It is a number of letters.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// String cannot be null.
+        /// </exception>
+        /// <exception cref="System.ArgumentException">
+        /// String cannot be empty.
+        /// </exception>
         public static int GetVowelCount(string str)
         {
             if (str == null)
             {
-                throw new ArgumentNullException("String cannot be null.");
+                throw new ArgumentNullException(str);
             }
-            else if(str=="")
+            else if (str.Length == 0)
             {
                 throw new ArgumentException("String cannot be empty.");
             }
-            int Sum = 0;            
+
+            int sum = 0;
             char[] letters = str.ToCharArray();
             foreach (char letter in letters)
             {
                 if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
                 {
-                    Sum++;
-                }                
-            }            
-            return Sum;           
+                    sum++;
+                }
+            }
+
+            return sum;
         }
     }
 }
--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -1,16 +1,34 @@
-using System;
+﻿using System;
 
 namespace Task2
 {
+    /// <summary>
+    /// This class is for finding correct number.
+    /// </summary>
     public static class IntegerExtensions
     {
+        /// <summary>
+        /// This method is for finding gcd.
+        /// </summary>
+        /// <param name="a">
+        /// It is a number a.
+        /// </param>
+        /// <param name="b">
+        /// It is a number b.
+        /// </param>
+        /// <returns>
+        /// It returns gcd.
+        /// </returns>
+        /// <exception cref="System.ArgumentException">
+        /// Two numbers cannot be 0 at the same time.
+        /// </exception>
         public static int GetGcd(int a, int b)
-        {  
+        {
             long first = a;
             long second = b;
             first = Math.Abs(first);
             second = Math.Abs(second);
-            int GCM = 1;
+            int gcd = 1;
             while (first != 0 & second != 0)
             {
                 if (first > second)
@@ -18,7 +36,7 @@ namespace Task2
                     first %= second;
                     if (first == 0)
                     {
-                        GCM = (int) second;
+                        gcd = (int)second;
                         break;
                     }
                 }
@@ -27,20 +45,23 @@ namespace Task2
                     second %= first;
                     if (second == 0)
                     {
-                        GCM = (int)first;
+                        gcd = (int)first;
                         break;
                     }
                 }
             }
-            if(first==0||second==0)
+
+            if (first == 0 || second == 0)
             {
-                GCM = (int)(first + second);
+                gcd = (int)(first + second);
             }
-            if(first == 0 && second == 0)
+
+            if (first == 0 && second == 0)
             {
                 throw new ArgumentException("Two numbers cannot be 0 at the same time.");
             }
-            return GCM;
+
+            return gcd;
         }
     }
 }
