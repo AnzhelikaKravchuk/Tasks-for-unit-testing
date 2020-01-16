9322967a05b535b176582e52afa9a9d2b0859f71
Zhenya Salash <ev.salash@gmail.com>
2020-01-16 15:12:36 +0300

    I finished task 2, but I am going to add commit a little bit later.

--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -5,9 +5,42 @@ namespace Task2
     public static class IntegerExtensions
     {
         public static int GetGcd(int a, int b)
-        {
-            // put your code here
-            throw new NotImplementedException();
+        {  
+            long first = a;
+            long second = b;
+            first = Math.Abs(first);
+            second = Math.Abs(second);
+            int GCM = 1;
+            while (first != 0 & second != 0)
+            {
+                if (first > second)
+                {
+                    first %= second;
+                    if (first == 0)
+                    {
+                        GCM = (int) second;
+                        break;
+                    }
+                }
+                else
+                {
+                    second %= first;
+                    if (second == 0)
+                    {
+                        GCM = (int)first;
+                        break;
+                    }
+                }
+            }
+            if(first==0||second==0)
+            {
+                GCM = (int)(first + second);
+            }
+            if(first == 0 && second == 0)
+            {
+                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+            return GCM;
         }
     }
 }
