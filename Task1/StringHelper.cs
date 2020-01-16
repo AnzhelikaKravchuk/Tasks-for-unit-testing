e884e47463e19787ed74fc6253d79b68928f13de
Kamargeldi <kmprojects1998@gmail.com>
2020-01-16 18:53:30 +0300

    Complete tasks.

--- Task1/StringHelper.cs
+++ Task1/StringHelper.cs
@@ -2,12 +2,44 @@
 
 namespace Task1
 {
+    /// <summary>
+    /// Class to work with string.
+    /// </summary>
     public static class StringHelper
     {
+        /// <summary>
+        /// Method to count vowels.
+        /// </summary>
+        /// <param name="str">Contains the string to search vowels.</param>
+        /// <returns>Returns the count of vowels in str.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when str is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when str is empty string.</exception>
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (str.Length == 0)
+            {
+                throw new ArgumentException("String length must be bigger than zero", nameof(str));
+            }
+
+            int ret = 0;
+            string vowels = "aouei";
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < vowels.Length; j++)
+                {
+                    if (str[i] == vowels[j])
+                    {
+                        ret++;
+                    }
+                }
+            }
+
+            return ret;
         }
     }
 }
--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -1,13 +1,56 @@
-using System;
+﻿using System;
 
 namespace Task2
 {
+    /// <summary>
+    /// Class to work with number theory.
+    /// </summary>
     public static class IntegerExtensions
     {
+        /// <summary>
+        /// Method to get GCD of two numbers.
+        /// </summary>
+        /// <param name="a">First integer number to find GCD.</param>
+        /// <param name="b">Second integer number to find GCD.</param>
+        /// <returns>Returns the GCD of a and b.</returns>
+        /// <exception cref="ArgumentException">Thrown when both parameters equal to zero.</exception>
         public static int GetGcd(int a, int b)
         {
-            // put your code here
-            throw new NotImplementedException();
+            long ua = Math.Abs((long)a);
+            long ub = Math.Abs((long)b);
+            if (ua == 0 && ub == 0)
+            {
+                throw new ArgumentException("Parameters must not be equal to zero");
+            }
+
+            while (ua != ub)
+            {
+                if (ua > ub)
+                {
+                    ua -= ub;
+                }
+                else
+                {
+                    ub -= ua;
+                }
+
+                if (ua == ub)
+                {
+                    return (int)ua;
+                }
+
+                if (ua == 0)
+                {
+                    return (int)ub;
+                }
+
+                if (ub == 0)
+                {
+                    return (int)ua;
+                }
+            }
+
+            return a;
         }
     }
 }
