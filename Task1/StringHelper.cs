f21c0975fda06b653ea9c3cc700eb0976789f79b
GhostPants12 <54960078+GhostPants12@users.noreply.github.com>
2020-01-17 00:36:41 +0300

    Complete Task1

--- Task1/StringHelper.cs
+++ Task1/StringHelper.cs
@@ -1,13 +1,41 @@
-﻿using System;
-
-namespace Task1
+﻿namespace Task1
 {
+    using System;
+
+    /// <summary>
+    /// An application entry point.
+    /// </summary>
     public static class StringHelper
     {
+        /// <summary>
+        /// Function that gets the number of vowels in the string.
+        /// </summary>
+        /// <param name="str">An input string.</param>
+        /// <exception cref="ArgumentException">Throws when the string is empty.</exception>
+        /// <exception cref="ArgumentNullException">Throws when the string is null.</exception>
+        /// <returns>The return is the number of vowels in the string.</returns>
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str), "String cannot be null.");
+            }
+
+            if (str.Length == 0)
+            {
+                throw new ArgumentException("String cannot be empty.");
+            }
+
+            int counter = 0;
+            foreach (char character in str)
+            {
+                if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
         }
     }
 }
