ca1aba2055e521a7982cc8d9b9237a2f8c464198
Darya Kunickaya <darya.kunickaya@gmail.com>
2020-01-16 15:31:32 +0300

    Done.

--- Task1/StringHelper.cs
+++ Task1/StringHelper.cs
@@ -2,12 +2,42 @@
 
 namespace Task1
 {
+    /// <summary>
+    /// The main class which have been testing.
+    /// </summary>
     public static class StringHelper
     {
+        /// <summary>
+        /// This method return the number (count) of vowels in the given string.
+        /// </summary>
+        /// <param name="str"> The input string will only consist of lower case letters and/or spaces. </param>
+        /// <returns> Number (count) of vowels. </returns>
+        /// <exception cref="ArgumentNullException()"> The input string cannot be null. </exception>
+        /// <exception cref="ArgumentException()"> The input string cannot be empty. </exception>
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (str == null)
+            {
+                throw new ArgumentNullException("Send an appropriate phrase, please.", new Exception());
+            }
+
+            if (str.Length == 0)
+            {
+                throw new ArgumentException("Send an appropriate phrase, please.");
+            }
+
+            int result = 0;
+            string vowels = "aeiou";
+
+            foreach (char letter in str)
+            {
+                if (vowels.Contains(letter, StringComparison.Ordinal))
+                {
+                    result++;
+                }
+            }
+
+            return result;
         }
     }
 }
--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -1,13 +1,27 @@
-using System;
+﻿using System;
 
 namespace Task2
 {
+    /// <summary>
+    /// The main class which have been testing.
+    /// </summary>
     public static class IntegerExtensions
     {
+        /// <summary>
+        /// Method for finding the greatest common divisor of two integers.
+        /// </summary>
+        /// <param name="a"> Number 1. </param>
+        /// <param name="b"> Number 2. </param>
+        /// <returns> Greatest common divisor. </returns>
+        /// <exception cref="ArgumentException()"> Two numbers cannot be 0 at the same time. </exception>
         public static int GetGcd(int a, int b)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+
+            return b == 0 ? Math.Abs(a) : GetGcd(b, a % b);
         }
     }
 }
