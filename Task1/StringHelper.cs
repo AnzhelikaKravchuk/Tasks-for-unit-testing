aee2729ffa54cce1ee37a0c2edf821a443ea3600
PashaFast <pashahit@gmail.com>
2020-01-18 18:13:29 +0300

    Add ruleset file, XML comments.

--- Task1/StringHelper.cs
+++ Task1/StringHelper.cs
@@ -2,16 +2,31 @@
 
 namespace Task1
 {
+    /// <summary>
+    /// Class StringHelper.
+    /// </summary>
     public static class StringHelper
     {
+        /// <summary>
+        /// Return the number (count) of vowels in the given string.
+        /// </summary>
+        /// <param name="str">Input string.</param>
+        /// <returns>number of vowels in the given string.</returns>
         public static int GetVowelCount(string str)
         {
-            if (str == null) throw new ArgumentNullException("String cannot be null.");
-            if (str.Equals(string.Empty)) throw new ArgumentException("String cannot be empty.");
+            if (str == null)
+            {
+                throw new ArgumentNullException("String cannot be null.");
+            }
+
+            if (str != null && str.Length == 0)
+            {
+                throw new ArgumentException("String cannot be empty.");
+            }
+
             int count = 0;
             for (int i = 0; i < str.Length; i++)
             {
-
                 if ((str[i] == 'a') || (str[i] == 'e') || (str[i] == 'i') || (str[i] == 'o') || (str[i] == 'u'))
                 {
                     count++;
--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -2,22 +2,59 @@ using System;
 
 namespace Task2
 {
+    /// <summary>
+    /// Class IntegerExtensions.
+    /// </summary>
     public static class IntegerExtensions
     {
+        /// <summary>
+        /// Algorithm for finding the GCD of two integers.
+        /// </summary>
+        /// <param name="a">Firrst int number.</param>
+        /// <param name="b">Second int number.</param>
+        /// <returns>GCD of two integers.</returns>
         public static int GetGcd(int a, int b)
         {
-            if ((a == 0) && (b == 0)) throw new ArgumentException("Two numbers cannot be 0 at the same time.");
-            if (a == int.MinValue && b == int.MaxValue) return 1;
-            if (a == int.MaxValue && b == int.MinValue) return 1;
-            if (a < 0) a = a * (-1);
-            if (b < 0) b = b * (-1);
-            if (a == 0) return b;
-            if (b == 0) return a;
-           
+            if ((a == 0) && (b == 0))
+            {
+                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+
+            if (a == int.MinValue && b == int.MaxValue)
+            {
+                return 1;
+            }
+
+            if (a == int.MaxValue && b == int.MinValue)
+            {
+                return 1;
+            }
+
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            if (a == 0)
+            {
+                return b;
+            }
+
+            if (b == 0)
+            {
+                return a;
+            }
 
             while (a != b)
-                if (a > b) a = a - b;
-                else b = b - a;
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+
             return a;
             throw new NotImplementedException();
         }
