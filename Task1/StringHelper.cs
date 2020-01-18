505038370be058f4b794576c7099d884a4c1ef1e
Vadim <vadim.katko11@gmail.com>
2020-01-18 20:01:27 +0300

    Trying to commit second task

--- Task1/StringHelper.cs
+++ Task1/StringHelper.cs
@@ -6,7 +6,31 @@ namespace Task1
     {
         public static int GetVowelCount(string str)
         {
-            // put your code here
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (str.Length == 0)
+            {
+                throw new ArgumentException("str.Length = 0", nameof(str));
+            }
+
+            int count = 0;
+            char[] mas = { 'a', 'e', 'i', 'o', 'u' };
+            foreach (char x in str)
+            {
+                foreach (char y in mas)
+                {
+                    if (x == y)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            return count;
             throw new NotImplementedException();
         }
     }
--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -6,7 +6,34 @@ namespace Task2
     {
         public static int GetGcd(int a, int b)
         {
-            // put your code here
+            if (a == 0 & b == 0)
+            {
+                throw new ArgumentException("a = 0 and b = 0");
+            }
+
+            if (a == 0)
+            {
+                return Math.Abs(b);
+            }
+
+            if (b == 0)
+            {
+                return Math.Abs(a);
+            }
+
+            for (; ;)
+            {
+                if ((a %= b) == 0)
+                {
+                    return Math.Abs(b);
+                }
+                else if
+                    ((b %= a) == 0)
+                {
+                    return Math.Abs(a);
+                }
+            }
+
             throw new NotImplementedException();
         }
     }
