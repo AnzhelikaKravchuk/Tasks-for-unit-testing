d74923a23fe5da14903e91ae53f76dd6fce4a3d7
PashaFast <pashahit@gmail.com>
2020-01-18 17:36:31 +0300

    Task done without XML-comments and analyizers

--- Task1/StringHelper.cs
+++ Task1/StringHelper.cs
@@ -6,7 +6,19 @@ namespace Task1
     {
         public static int GetVowelCount(string str)
         {
-            // put your code here
+            if (str == null) throw new ArgumentNullException("String cannot be null.");
+            if (str.Equals(string.Empty)) throw new ArgumentException("String cannot be empty.");
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if ((str[i] == 'a') || (str[i] == 'e') || (str[i] == 'i') || (str[i] == 'o') || (str[i] == 'u'))
+                {
+                    count++;
+                }
+            }
+
+            return count;
             throw new NotImplementedException();
         }
     }
--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -6,7 +6,19 @@ namespace Task2
     {
         public static int GetGcd(int a, int b)
         {
-            // put your code here
+            if ((a == 0) && (b == 0)) throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            if (a == int.MinValue && b == int.MaxValue) return 1;
+            if (a == int.MaxValue && b == int.MinValue) return 1;
+            if (a < 0) a = a * (-1);
+            if (b < 0) b = b * (-1);
+            if (a == 0) return b;
+            if (b == 0) return a;
+           
+
+            while (a != b)
+                if (a > b) a = a - b;
+                else b = b - a;
+            return a;
             throw new NotImplementedException();
         }
     }
