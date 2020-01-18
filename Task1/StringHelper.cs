96849a2c4403d1410ab494c59dea94d84106c954
GreenAcidy <FirstKnight@list.ru>
2020-01-19 00:04:49 +0300

    Task done

--- Task1/StringHelper.cs
+++ Task1/StringHelper.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿// <copyright file="StringHelper.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace Task1
 {
+    using System;
+
+    /// <summary >
+    /// Сlass StringHelper.
+    /// </summary >
     public static class StringHelper
     {
+        /// <summary >
+        /// Algorith of finding all vowels in a string.
+        /// </summary >
+        /// <param  name = " str " > Initial string. </param >
+        /// <exception cref="ArgumentNullException">Throw when string is null.</exception>
+        /// <exception cref="ArgumentException">Throw when string has no characters.</exception>
+        /// <returns>Number of vowels in a string.  </returns>
         public static int GetVowelCount(string str)
         {
-            // put your code here
+            int result = 0;
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (str.Length == 0)
+            {
+                throw new ArgumentException("hthtr");
+            }
+
+            foreach (char symbol in str)
+            {
+                if (symbol == 'a')
+                {
+                    result++;
+                }
+                else if (symbol == 'e')
+                {
+                    result++;
+                }
+                else if (symbol == 'i')
+                {
+                    result++;
+                }
+                else if (symbol == 'o')
+                {
+                    result++;
+                }
+                else if (symbol == 'u')
+                {
+                    result++;
+                }
+            }
+
+            return result;
             throw new NotImplementedException();
         }
     }
--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -1,12 +1,39 @@
-using System;
+// <copyright file="IntegerExtensions.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace Task2
 {
+    using System;
+
+    /// <summary >
+    /// Ñlass StringHelper.
+    /// </summary >
     public static class IntegerExtensions
     {
+        /// <summary >
+        /// Algorith of finding the GCD of two integer.
+        /// </summary >
+        /// <param  name = " a " > First int number. </param >
+        /// <param  name = " b " > Second int number. </param >
+        /// <exception cref="ArgumentException">Two numbers are equal 0 at the same time.</exception>
+        /// <returns>GCD of two integer.  </returns>
         public static int GetGcd(int a, int b)
         {
-            // put your code here
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+
+            if (b == 0)
+            {
+                return Math.Abs(a);
+            }
+            else
+            {
+                return GetGcd(b, a % b);
+            }
+
             throw new NotImplementedException();
         }
     }
